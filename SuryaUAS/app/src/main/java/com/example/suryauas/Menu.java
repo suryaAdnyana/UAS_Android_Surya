41c29b66bdeb9cf6597075cb9b071231eb3979a5
YogiWiguna <58981369+YogiWiguna@users.noreply.github.com>
2020-06-01 20:03:37 +0800

    UAS Android
    Membuat UAS

---
+++ SuryaUAS/app/src/main/java/com/example/suryauas/Menu.java
@@ -0,0 +1,26 @@
+package com.example.suryauas;
+
+public class Menu {
+
+    private String nama;
+    private String harga;
+    private String gambar;
+
+    public Menu(String datanama, String dataharga, String datagambar){
+        nama=datanama;
+        harga=dataharga;
+        gambar=datagambar;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public String getHarga() {
+        return harga;
+    }
+
+    public String getGambar() {
+        return gambar;
+    }
+}
